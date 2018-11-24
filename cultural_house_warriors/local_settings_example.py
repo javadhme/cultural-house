460b5e5761a22d3646690947a4a9fbbc0360ebdf
imankarimi <imankarimi.mail@gmail.com>
2018-11-24 14:14:49 +0330

    add settings

---
+++ cultural_house_warriors/local_settings_example.py
@@ -0,0 +1,10 @@
+APP_DEBUG = True
+APP_KEY = ''
+
+# Database Connection
+DB_ENGINE = 'django.db.backends.mysql'
+DB_HOST = '127.0.0.1'
+DB_PORT = '3306'
+DB_DATABASE = 'homestead'
+DB_USERNAME = 'homestead'
+DB_PASSWORD = 'secret'
