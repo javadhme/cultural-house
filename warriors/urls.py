edad86a51e55dd96dafcf50cb55df4b48642469a
imankarimi <imankarimi.mail@gmail.com>
2018-12-08 21:15:59 +0330

    set form

--- warriors/urls.py
+++ warriors/urls.py
@@ -1,7 +1,9 @@
 from warriors import views
 from django.urls import path
+from warriors.views import Warriors
+
 
 urlpatterns = [
     path('', views.index, name='home'),
-    path('warriors/', views.warriors, name='warriors'),
+    path('warriors/', Warriors.as_view(), name='warriors'),
 ]
--- warriors/views.py
+++ warriors/views.py
@@ -1,4 +1,5 @@
 from django.shortcuts import render
+from django.views.generic import TemplateView
 
 from warriors.models import MEMBERSHIP_TYPE_CHOICES, SITUATION_CHOICES, Report, Soldier
 
@@ -10,6 +11,31 @@ def index(request):
 def warriors(request):
     context = dict(
         memberships=MEMBERSHIP_TYPE_CHOICES,
-        situations=SITUATION_CHOICES
+        situations=SITUATION_CHOICES,
+        soldiers_type=Soldier.TYPE_CHOICES,
+        soldiers_count=range(1, 6)
     )
     return render(request, 'home/warriors.html', context)
+
+
+class Warriors(TemplateView):
+    template_name = 'home/warriors.html'
+
+    def get(self, request, **kwargs):
+        context = dict(
+            memberships=MEMBERSHIP_TYPE_CHOICES,
+            situations=SITUATION_CHOICES,
+            soldiers_type=Soldier.TYPE_CHOICES,
+            soldiers_count=range(1, 6)
+        )
+        return render(request, self.template_name, context)
+
+    def post(self, request):
+        context = dict(
+            memberships=MEMBERSHIP_TYPE_CHOICES,
+            situations=SITUATION_CHOICES,
+            soldiers_type=Soldier.TYPE_CHOICES,
+            soldiers_count=range(1, 6)
+        )
+        print(request.POST)
+        return render(request, self.template_name, context)
